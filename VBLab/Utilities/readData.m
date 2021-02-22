8a01eb1d753371c8edd5f89948682863d9023673
nghia vp07 <nghia.nguyen@sydney.edu.au>
2021-02-22 23:08:58 +1100

    update readdata

--- VBLab/Utilities/readData.m
+++ VBLab/Utilities/readData.m
@@ -1,50 +1,112 @@
-function data = readData(dataName, varargin)
+function data_out = readData(dataName, varargin)
 
 % Initialize additional options
 Intercept = true;  
-Length = 0;
-Normalized = true;
+Length = NaN;
+Normalized = false;
+Index = '';
+Type = 'Matrix';
+RealizedMeasure = '';
 
 % Load user's options
 if nargin > 1
     %Parse additional options
-    paramNames = {'Intercept'     'Length'     'Normalized'};
-    paramDflts = { Intercept      Length       Normalized};
+    paramNames = {'Intercept'     'Length'     'Normalized' ...
+                  'Index'         'Type'       'RealizedMeasure'};
+    paramDflts = { Intercept      Length       Normalized ...
+                   Index          Type         RealizedMeasure };
 
     [Intercept,...
      Length,...
-     Normalized] = internal.stats.parseArgs(paramNames, paramDflts, varargin{:});                
+     Normalized,...
+     Index,...
+     Type,...
+     RealizedMeasure] = internal.stats.parseArgs(paramNames, paramDflts, varargin{:});                
 end 
 
 % Load built-in datasets
 datatype = '';
 switch dataName
-    case 'LabourForce'
+    % Abalon data
+    case 'Abalon'
         datatype = 'Cross-Sectional';
-        data = load('LabourForce.mat');
+        data = load('Abalon.mat');
+        data_mat = data.data;
         
+    % DirectMarketing data
+    case 'DirectMarketing'
+        datatype = 'Cross-Sectional';
+        data = load('DirectMarketing.mat');
+        data_mat = data.data;
+        if(Normalized)
+            norm_col = [1,2,3,12];
+            data_mat(:,norm_col) = zscore(data_mat(:,norm_col));
+        end
+        
+    % GermanCredit data
     case 'GermanCredit'
         datatype = 'Cross-Sectional';
         data = load('GermanCredit.mat');
-        if (Normalized)
-            data.X = [zscore(data.X(:,1:15)),data.X(:,16:end)];
+        data_mat = data.data;
+        if(Normalized)
+            data_mat = [zscore(data.X(:,1:15)),data.X(:,16:end)];
         end
         
-    case 'SP500'
-        datatype = 'TimeSeries';
-        data = load('StockIndex.mat');
+    % LabourForce data
+    case 'LabourForce'
+        datatype = 'Cross-Sectional';
+        data = load('LabourForce.mat');
+        data_mat = data.data;
         
-        if Length > 0
-            
+        
+    % RealizedLibrary data
+    case 'RealizedLibrary'
+        datatype = 'TimeSeries';
+        data = load('RealizedLibrary.mat');
+        % If length is specified
+        if(isempty(Index))
+            error('At least one index must be specified!')
+        else
+            data_mat = data.(Index).open_to_close*100;  
+            if Length > 0 
+                T = Length;
+                if Length <= length(data_mat)
+                    data_mat = data_mat(end-T+1:end);
+                else
+                    error('The Length argument must be smaller than the lenght of the time series!')
+                end
+            end
+        end
+        
+        if(~isempty(RealizedMeasure))
+            num_obs = length(data_mat);
+            data_out.return = data_mat;
+            if iscell(RealizedMeasure)
+                num_realized = length(RealizedMeasure);
+                for i = 1:num_realized
+                    data_out.(RealizedMeasure{i}) = data.(Index).(RealizedMeasure{i})(end-num_obs+1:end)*100^2;
+                end
+            else
+                data_out.(RealizedMeasure) = data.(Index).(RealizedMeasure)(end-num_obs+1:end)*100^2;    
+            end
+        else
+            data_out = data_mat;
         end
 end
+
 
 %% Check additional options
 % If a column of 1 is added to the matrix X of cross-sectional data (default)
 if strcmp(datatype,'Cross-Sectional') && Intercept
-    data.X = [ones(size(data.X,1),1),data.X];
+    data_mat = [ones(size(data_mat,1),1),data_mat];
+    VarNames = ['Intercept',data.VarNames];
+    data_out = data_mat;
+    if strcmp(Type,'Table')
+        data_table = array2table(data_mat);
+        data_table.Properties.VariableNames = VarNames;
+        data_out = data_table;
+    end
 end
-
 
 end
 
