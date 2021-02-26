68955cba1a6f67299ddd2c12e0d29fe1d8e36c58
nghia vp07 <nghia.nguyen@sydney.edu.au>
2021-02-26 22:34:29 +1100

    update examples

--- Example/NAGVAC_Logistics_Model_Object.m
+++ Example/NAGVAC_Logistics_Model_Object.m
@@ -20,50 +20,34 @@ Mdl = LogisticRegression(n_features,...
 					    'Prior',{'Normal',[0,50]});
                           
 %% Run Cholesky GVB with random initialization
-theta_init = Mdl.initParams('MLE',labour); 
-Post_VAFC = NAGVAC(Mdl,labour,...
-                  'MeanInit',theta_init,... % Initial values of variational mean
+Post_NAGVAC = NAGVAC(Mdl,labour,...
                   'NumSample',200,...       % Number of samples to estimate gradient of lowerbound
-                  'LearningRate',0.001,...  % Learning rate
+                  'LearningRate',0.005,...  % Learning rate
                   'MaxPatience',20,...      % For Early stopping
                   'MaxIter',10000,...       % Maximum number of iterations
                   'GradientMax',200,...     % For gradient clipping    
-                  'WindowSize',30, ...      % Smoothing window for lowerbound
+                  'WindowSize',50, ...      % Smoothing window for lowerbound
                   'LBPlot',true);           % Dont plot the lowerbound when finish
              
-
-%% It is useful to compare the approximate posterior density to the true density obtain by MCMC
-Post_MCMC = MCMC(Mdl,labour,...
-                 'NumMCMC',50000,...
-                 'ParamsInit',theta_init,...
-                 'Verbose',1000);       % Display after each 1000 iterations
-             
-%% Compare densities by CGVB and MCMC
-% Get posterior mean and trace plot for a parameter to check the mixing 
-[mcmc_mean,mcmc_std,mcmc_chain] = Post_MCMC.getParamsMean('BurnInRate',0.5,...
-                                                          'PlotTrace',1);
-
-% Plot density
-fontsize  = 20;
-
-% Extract variation mean and variance
-mu_vb     = Post_VAFC.Post.mu;
-sigma2_vb = Post_VAFC.Post.sigma2;
-
-% Plot only variational distribution for 8 parameters 
+%% Plot variational distributions and lowerbound 
 figure
-for i = 1:8 
+% Extract variation mean and variance
+mu_vb     = Post_NAGVAC.Post.mu;
+sigma2_vb = Post_NAGVAC.Post.sigma2;
+
+% Plot the variational distribution for the first 8 parameters
+for i=1:n_features
     subplot(3,3,i)
-    xx = mcmc_mean(i)-4*mcmc_std(i):0.002:mcmc_mean(i)+4*mcmc_std(i);
-    yy_mcmc = ksdensity(mcmc_chain(:,i),xx);    
-    yy_vb = normpdf(xx,mu_vb(i),sqrt(sigma2_vb(i)));
-    plot(xx,yy_mcmc,'-k',xx,yy_vb,'--b','LineWidth',1.5)
-    line([theta_init(i) theta_init(i)],ylim,'LineWidth',1.5,'Color','r')    
-    str = ['\theta_', num2str(i)];   
-    title(str,'FontSize', fontsize)
-    legend('MCMC','VB')
+    vbayesPlot('Density',{'Normal',[mu_vb(i),sigma2_vb(i)]})
+    grid on
+    title(['\theta_',num2str(i)])
+    set(gca,'FontSize',15)
 end
+
+% Plot the smoothed lower bound
 subplot(3,3,9)
-plot(Post_VAFC.Post.LB_smooth,'LineWidth',1.5)
-title('Lower bound','FontSize', fontsize)          
+plot(Post_NAGVAC.Post.LB_smooth,'LineWidth',2)
+grid on
+title('Lower bound')
+set(gca,'FontSize',15)     
               
